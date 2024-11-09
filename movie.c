da3a964d6ef8ca6a3ee700edb3cb5c70b5283435
tryst1129 <tristanlarson@gmail.com>
2024-11-09 11:56:14 -0600

    yesterday night forgot to commit

--- movie.c
+++ movie.c
@@ -14,7 +14,10 @@ int find_movie(FILE *movies, char find[]) {
                 for (int j = 1; j < strlen(find); j++) {
                     if (buffer[i+j] != find[j]) found = 0;
                 }
-                if (found) return lineCount;
+                if (found) {
+                    // TODO rewind pointer to file start
+                    return lineCount/5+1; // Return the movie number
+                }
             }
         }
 
@@ -26,17 +29,20 @@ int find_movie(FILE *movies, char find[]) {
 
 }
     
+void print_movie(FILE *movies, int movieNum) {
 
-    
+}
+
+
+
+
 int main(void) {
 
 FILE *movies = fopen("movies", "r");
+
 char find[32];
 printf("Enter movie to find: ");
 fgets(find, 32, stdin);
-// for (int i = 0; i < strlen(find); i++) {
-//     if (find[i] == '\n') find[i] = '\0';
-// }
 find[strcspn(find, "\n")] = '\0';  // Remove newline from find[]
 
 if (movies == NULL) {
@@ -44,6 +50,6 @@ if (movies == NULL) {
     return 1;
 }
 
-printf("line: %d", find_movie(movies, find));
+printf("Movie Number: %d", find_movie(movies, find));
 fclose(movies);
 }
